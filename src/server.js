2a797596e68ee66acebf83f5fd27a5d9a3a5e48f
Bobolan123 <127038290+Bobolan123@users.noreply.github.com>
2023-12-06 08:51:35 +0700

    generate model/migration/seeder of sequalize

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import initWebRoutes from "./routes/web";
 import configViewEngine from "./config/viewEngine";
 require('dotenv').config()
 import bodyParser from 'body-parser'
+// import connection from "./config/connectDB";
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -12,6 +13,9 @@ configViewEngine(app)
 //config body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//test connection DB
+// connection()
 
 //init web route
 initWebRoutes(app)
