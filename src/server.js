59312483326379f6226e438825fc5b607c718bb7
Bobolan123 <127038290+Bobolan123@users.noreply.github.com>
2023-12-12 09:09:15 +0700

    API and test

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser'
 // import connection from "./config/connectDB";
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 //config view engine
 configViewEngine(app)
 
