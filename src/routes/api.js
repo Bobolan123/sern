04237662c22b64b1666bef7311bd626745165459
Bobolan123 <127038290+Bobolan123@users.noreply.github.com>
2023-12-12 21:14:21 +0700

    get data through api

---
+++ src/routes/api.js
@@ -0,0 +1,17 @@
+import express from "express";
+import apiController from "../controller/apiController";
+
+const router = express.Router();
+/**
+ *
+ * @param {*} app -express app
+ */
+const initApiRoutes = (app) => {
+  //rest api
+  //GET --R, POST --C, PUT-U, DELETE - D
+  router.get("/api/test-api", apiController.testApi);
+  router.post("/register", apiController.handleRegister);
+  return app.use("/api/", router);
+};
+
+export default initApiRoutes;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import initWebRoutes from "./routes/web";
+import initApiRoutes from "./routes/api";
 import configViewEngine from "./config/viewEngine";
 require('dotenv').config()
 import bodyParser from 'body-parser'
 // import connection from "./config/connectDB";
+import confCors from "./config/cors";
 
 const app = express()
 const PORT = process.env.PORT || 3001
+
+//config cors
+confCors(app)
+
 //config view engine
 configViewEngine(app)
 
@@ -19,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //init web route
 initWebRoutes(app)
+initApiRoutes(app)
 
 app.listen(PORT, () => {
     console.log("PORT", PORT)
